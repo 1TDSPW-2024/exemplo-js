df19796d78728daf3ceeaab17154ee0d2577d639
CauaMachad0 <caua.marcelo.machado@outlook.com>
2024-05-16 19:37:22 -0300

    .

---
+++ js/script-sucesso.js
@@ -0,0 +1,7 @@
+
+//Recuperando o objeto validado do localStorage.
+const usuario = JSON.parse(localStorage.getItem("usuario-validado"));
+console.log(usuario.emailUsuario); 
+
+const chaveString = localStorage.getItem("chave");
+console.log(chaveString); 
